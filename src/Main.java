438e9a59853ac63e4ec09b040a0904a6bc9af6a7
Alejandro Juarez <ja24001@esfe.agape.edu.sv>
2025-06-04 06:27:16 -0600

    Mi primer commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+/**
+ * Clase principal para demostrar el uso de una lista enlazada simple.
+ */
+public class Main {
+
+    /**
+     * Método principal que demuestra operaciones básicas en una lista enlazada simple.
+     *
+     * @param args Argumentos de la línea de comandos.
+     */
+    public static void main(String[] args) {
+        ListaEnlazadaSimple lista = new ListaEnlazadaSimple();
+
+        // Demostración de operaciones con la lista
+        System.out.println("Demostracion de la lista enlazada simple");
+        System.out.println("Es una lista vacia? " + lista.esListaVacia());
+
+        // Inserción de elementos
+        lista.insertarAlInicio(10);
+        lista.insertarAlInicio(20);
+        lista.insertarAlInicio(30);
+        lista.insertarAlFinal(5);
+
+        // Eliminación de elementos
+        lista.eliminar(20);
+        lista.eliminarAlInicio();
+
+        // Búsqueda de elementos
+        System.out.println("EXISTE EL NUMERO 20? " + lista.buscar(20));
+        System.out.println("EXISTE EL NUMERO 10? " + lista.buscar(10));
+
+        // Información de la lista
+        System.out.println("Tamanio de la lista: " + lista.tamanio());
+        System.out.println("Informe de memoria: " + lista.informacionMemoria());
+
+        // Impresión de la lista
+        lista.imprimir();
+    }
+}
